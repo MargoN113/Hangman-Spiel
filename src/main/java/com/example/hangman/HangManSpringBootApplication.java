16a2137d3db50b1c0828a016d0af3a64bcc57ef4
Margarita Nefedova <margonefedova2004@gmail.com>
2025-03-08 21:19:38 +0100

    Add files via upload

---
+++ src/main/java/com/example/hangman/HangManSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.hangman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangManSpringBootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HangManSpringBootApplication.class, args);
+	}
+
+}
